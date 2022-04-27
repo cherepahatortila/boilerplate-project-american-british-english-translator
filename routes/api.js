80c0ae860f625fab7e0c97aa7e808816254417aa
Iryna <59018238+cherepahatortila@users.noreply.github.com>
2022-04-27 21:39:10 +0300

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,18 @@
 const Translator = require('../components/translator.js');
 
 module.exports = function (app) {
-  
-  const translator = new Translator();
-
   app.route('/api/translate')
-    .post((req, res) => {
-      
-    });
+ .post((req, res) => {   
+  const translator = new Translator(req.body.text,req.body.locale);
+  if(req.body.text=="")
+   res.send({ error: 'No text to translate' })
+  else if(!req.body.text||!req.body.locale)
+   res.send({ error: 'Required field(s) missing' })
+  else if(req.body.locale!='american-to-british'&&req.body.locale!='british-to-american')
+   res.send({ error: 'Invalid value for locale field' })
+  else if(req.body.text==translator.translate())
+   res.send({text:req.body.text, translation:'Everything looks good to me!' })
+  else
+    res.send({text:req.body.text, translation:translator.translate()})
+  });
 };
